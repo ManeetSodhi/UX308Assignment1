0be24f290edad0e4e9205e2aab0e918cd7a825fc
ManeetSodhi <144968865+ManeetSodhi@users.noreply.github.com>
2026-02-27 01:28:11 +0000

    -change to Maneet Hortons menu

--- Order.js
+++ Order.js
@@ -10,24 +10,62 @@ export function clearInput(){
 
 function welcoming() {
   let aReturn = [];
-  currentState = reserving;
-  aReturn.push("Welcome to Rich's Acton Rapid Test.");
-  aReturn.push("Would you like to reserve a rapid test kit?");
+  currentState = ordering;
+  aReturn.push("Welcome to Maneet Hortons.");
+  aReturn.push("Would you like to place an order? (please answer yes/no only)");
   return aReturn;
 }
 
-function reserving(sInput) {
+function ordering(sInput) {
   let aReturn = [];
-  currentState = welcoming
-  if (sInput.toLowerCase().startsWith('y')) {
-    aReturn.push(`Your rapid test is reserved`);
-    let d = new Date();
-    d.setMinutes(d.getMinutes() + 120);
-    aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`);
+  currentState = modifying
+  if (sInput.toLowerCase().startsWith('yes')) {
+    aReturn.push(`Great!`);
+    //let d = new Date();
+    //d.setMinutes(d.getMinutes() + 120);
+    aReturn.push(`Would you like an iced or hot coffee?`);
   } else {
-    aReturn.push("Thanks for trying our reservation system");
-    aReturn.push("Maybe next time");
+    currentState = welcoming
+    aReturn.push("Thanks for trying our ordering system");
+    aReturn.push("See you next time");
   }
   return aReturn;
 }
 
+let modification = "";
+
+function modifying(sInput) {
+  let aReturn = [];
+  currentState = confirming
+  if (sInput.toLowerCase().startsWith('ice')) {
+    modification = "Iced";
+    aReturn.push(`What size would you like? (S/M/L)`);
+  } else if (sInput.toLowerCase().startsWith('hot')) {
+    modification = "Hot";
+    aReturn.push(`What size would you like? (S/M/L)`);
+  } else {
+    currentState = ordering;
+    aReturn.push("Please try again");
+  }
+  return aReturn;
+}
+
+
+function confirming(sInput) {
+  let aReturn = [];
+  currentState = welcoming
+  if (sInput.toLowerCase().startsWith('s')) {
+    aReturn.push(`You ordered a Small ${modification} Coffee`);
+    aReturn.push(`Please order again!`);
+  } else if (sInput.toLowerCase().startsWith('m')) {
+    aReturn.push(`You ordered a Medium ${modification} Coffee`);
+    aReturn.push(`Please order again!`);
+  } else if (sInput.toLowerCase().startsWith('l')) {
+    aReturn.push(`You ordered a Large ${modification} Coffee`);
+    aReturn.push(`Please order again!`);
+  } else {
+    currentState = modifying;
+    aReturn.push("Please try again");
+  }
+  return aReturn;
+}
